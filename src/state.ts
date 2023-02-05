ced6f03e911e26ac5b2d041a36f2eb907ce7cac3
Ami Hazbany <ami@orbs.com>
2023-02-05 12:38:05 +0200

    first commit server side for ton dao vote

---
+++ src/state.ts
@@ -0,0 +1,46 @@
+import {TxData,VotingPower, Votes, ProposalResults, ProposalInfo} from "./types";
+
+export class State {
+
+    private txData: TxData = {tx: [], toLt: undefined};
+    private votingPower: VotingPower = {};
+    private votes: Votes = {};
+    private proposalResults: ProposalResults | undefined;
+    private proposalInfo: ProposalInfo | undefined;
+    private lastUpdateTime: Number | undefined;
+
+
+    getState() {
+
+        return {
+            txData: this.txData,
+            votingPower: this.votingPower,
+            votes: this.votes,
+            proposalResults: this.proposalResults,
+            proposalInfo: this.proposalInfo,
+            lastStateUpdate: this.lastUpdateTime
+        }
+    }
+
+    getProposalResults() {
+        return this.proposalResults;
+    }
+
+    getProposalInfo() {
+        return this.proposalInfo;
+    }
+
+    setState(txData: TxData, votingPower: VotingPower, votes: Votes, proposalResults: ProposalResults) {
+        console.log(`updating state ...`);
+        
+        this.txData = txData;
+        this.votingPower = votingPower;
+        this.votes = votes;
+        this.proposalResults = proposalResults;
+        this.lastUpdateTime = Date.now();
+    }
+
+    setProposalInfo(proposalInfo: ProposalInfo) {
+        this.proposalInfo = proposalInfo;
+    }
+}
