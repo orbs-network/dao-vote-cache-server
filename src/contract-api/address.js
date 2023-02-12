329b6e595afcbdb72443b7545a08bcc29636d931
Ami Hazbany <ami@orbs.com>
2023-02-12 12:42:44 +0200

    use old contract for dbg

--- src/contract-api/address.js
+++ src/contract-api/address.js
@@ -1,6 +1,7 @@
 import { Address } from "ton";
 
+// EQDGU1GC3wjaAqPPXfV5uOZN3AiKT_W2pgPA-pQywX4zjBX_
 export const votingContract = Address.parse(
-    "EQDGU1GC3wjaAqPPXfV5uOZN3AiKT_W2pgPA-pQywX4zjBX_"
+    "EQANFJgI3ahDXtSXEGkWYNR7Lqr6dBrVtrCTem5_DuQtTcDT"
 );
   
