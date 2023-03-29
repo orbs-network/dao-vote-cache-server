18b52c84082b858ad10cd92ebe836f78bb94703c
Ami Hazbany <ami@orbs.com>
2023-03-29 19:29:48 +0300

    change contract

--- src/contract-api/config.js
+++ src/contract-api/config.js
@@ -2,7 +2,7 @@ import { Address } from "ton";
 
 
 export const votingContract = Address.parse(
-    "EQDQgxkz1Q6vg5ddRqT6w2_0lc0_DY6Ck6M9cdqzjiCmO7Ek" //"EQD0b665oQ8R3OpEjKToOrqQ9a9B52UnlY-VDKk73pCccvLr"
+    "EQD0b665oQ8R3OpEjKToOrqQ9a9B52UnlY-VDKk73pCccvLr"
 );
 
 export const VOTE_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
