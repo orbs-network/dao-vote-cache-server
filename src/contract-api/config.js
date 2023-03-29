ef11096977bfcc301febf8f0dcf6c37819a2b255
Ami Hazbany <ami@orbs.com>
2023-03-29 08:23:06 +0300

    add rename file

---
+++ src/contract-api/config.js
@@ -0,0 +1,8 @@
+import { Address } from "ton";
+
+
+export const votingContract = Address.parse(
+    "EQDOpqfdHHfB0YvPqPupyPnA8HHF0V1aoDaZEStO1fG0nzdb"
+);
+
+export const VOTE_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
