cb9151546ab10aa0f22419612d01da405cb12d15
Ami Hazbany <ami@orbs.com>
2023-03-29 09:37:24 +0300

    new contract

--- src/contract-api/config.js
+++ src/contract-api/config.js
@@ -2,7 +2,7 @@ import { Address } from "ton";
 
 
 export const votingContract = Address.parse(
-    "EQDOpqfdHHfB0YvPqPupyPnA8HHF0V1aoDaZEStO1fG0nzdb"
+    "EQD0b665oQ8R3OpEjKToOrqQ9a9B52UnlY-VDKk73pCccvLr"
 );
 
 export const VOTE_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
